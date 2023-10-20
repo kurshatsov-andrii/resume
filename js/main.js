22da1da8165a21257a63f6710b566823ccc664eb
Andrii Kurshatsov <andreswebit@ukr.net>
2023-10-20 13:22:17 +0300

    Update id

--- js/main.js
+++ js/main.js
@@ -31,14 +31,17 @@ function scrollActive() {
 
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute("id");
+    const sectionTop = current.offsetTop - 10;
+    let sectionId = "";
+    //console.log(sectionId);
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionId = current.getAttribute("id");
       document
         .querySelector(".nav__menu a[href*=" + sectionId + "]")
         .classList.add("active-link");
     } else {
+      sectionId = current.getAttribute("id");
       document
         .querySelector(".nav__menu a[href*=" + sectionId + "]")
         .classList.remove("active-link");
